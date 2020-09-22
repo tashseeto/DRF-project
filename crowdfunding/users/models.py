7ed38f7edb438ceb8724e90a8ffab175ebaac1c6
Natasha Seeto <tashseeto@gmail.com>
2020-09-22 19:16:07 +1000

    updated user model and serializers

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    image = models.URLField()
+    date_created = models.DateTimeField()
+
 
     def __str__(self):
         return self.username
--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -7,6 +7,10 @@ class CustomUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
     password = serializers.CharField(write_only = True)
+    first_name = serializers.CharField(max_length=200)
+    last_name = serializers.CharField(max_length=200)
+    image = serializers.URLField()
+    date_created = serializers.DateTimeField()
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
