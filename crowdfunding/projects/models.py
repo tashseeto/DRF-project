1cae3e605a0d663142d96b0020446ef9b4b7cfa0
Natasha Seeto <tashseeto@gmail.com>
2020-10-06 18:02:31 +1000

    model migrations including heroku

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -6,11 +6,11 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     goal = models.IntegerField()
-    total_raised = models.IntegerField()
+    total_raised = models.IntegerField(default=0)
     image = models.URLField()
     is_open = models.BooleanField()
-    date_created = models.DateTimeField()
-    date_end = models.DateTimeField()
+    date_created = models.DateTimeField(default=timezone.now())
+    date_end = models.DateTimeField(default=timezone.now())
     owner = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
